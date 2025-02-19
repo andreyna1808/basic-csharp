bef56b214ca6e15824fa934769b7e97b25a0b386
andreyna.carvalho <andreyna.carvalho@iatec.com>
2025-02-19 09:20:19 -0300

    Construtores

--- POO/Heranca.cs
+++ POO/Heranca.cs
@@ -2,22 +2,45 @@
 
 using System;
 
-class Principal {
-    static void Main() {
+class Principal
+{
+    static void Main()
+    {
         Felinos gatos = new Felinos();
         Console.WriteLine(gatos.vida); // 100
-
     }
 }
 
-class Animais {
+class Animais
+{
     public int vida = 100;
     public string especie;
     private int _peso = 0;
 }
 
-class Felinos: Animais { // Ela herda da classe de animais, ou seja possui vida e especie. Menos _peso pq é uma classe privada
-
+class Felinos
+    : Animais { // Ela herda da classe de animais, ou seja possui vida e especie. Menos _peso pq é uma classe privada
 }
 
 // Obs.: Construtores não são herdados
+
+class Classe1
+{
+    public Classe1(int valor)
+    {
+        Console.WriteLine("Construtor da classe 1");
+    }
+}
+
+class Classe2 : Classe1
+{
+    static int valor = 10;
+
+    // O construtor Classe2 chama o construtor Classe1
+    // :base é usado para chamar o construtor da classe pai
+    public Classe2()
+        : base(valor)
+    {
+        Console.WriteLine("Construtor da classe 2");
+    }
+}
